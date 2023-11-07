a75930222d3807cd338767816827df354bdc20b7
AmeyTari2002 <116430042+AmeyTari2002@users.noreply.github.com>
2023-11-07 16:47:53 +0530

    Add files via upload
    AMEY TARI DIGIT

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DynamicTable from './dynamicTable';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicTable/>
+    </div>
+  );
+}
+
+export default App;
